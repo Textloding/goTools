38d301e6745fd462ec4c13e7f7a08a58c300b298
LieHuoZhenJin <taiyiit@163.com>
2023-06-04 21:31:54 +0800

    HorJin

--- utils/time.go
+++ utils/time.go
@@ -4,6 +4,10 @@ import (
 	"time"
 )
 
+const (
+	TIMETEMPLATE1 = "2006-01-02 15:04:05" //常规类型
+)
+
 // 当前时间
 func GetTime() time.Time {
 	return time.Now()
@@ -12,4 +16,9 @@ func GetTime() time.Time {
 // 当前时间戳
 func GetUnixTime() int64 {
 	return time.Now().Unix()
+}
+
+// 格式化为:2006-01-02 15:04:05
+func GetNormalTimeString(t time.Time) string {
+	return t.Format(TIMETEMPLATE1)
 }
