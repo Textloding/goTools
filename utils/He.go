9de751a018fa56c817e02c309486cecb672749d0
hejiahui <3044662106@qq.com>
2024-05-17 14:55:31 +0800

    工作第三天

--- utils/He.go
+++ utils/He.go
@@ -56,3 +56,20 @@ func work(working bool) string {
         return "别摸了"
     }
 }
+
+func thisDay() {
+    switch time.Now().Weekday() {
+    case time.Monday:
+        fmt.Println("今天是星期一")
+    case time.Tuesday:
+        fmt.Println("今天是星期二")
+    case time.Wednesday:
+        fmt.Println("今天是星期三")
+    case time.Thursday:
+        fmt.Println("今天是星期四")
+    case time.Friday:
+        fmt.Println("今天是星期五")
+    case time.Saturday:
+        fmt.Println("今天是星期六")
+    }
+}
