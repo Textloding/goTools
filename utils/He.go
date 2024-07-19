a64656daa648a60aada0f2011131a7a1167f932c
hejiahui <3044662106@qq.com>
2024-07-19 09:13:03 +0800

    提交

--- utils/He.go
+++ utils/He.go
@@ -781,3 +781,30 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
         panic(err)
     }
 }
+
+// ArticlesFormData 创建博文表单数据
+type ArticlesFormData struct {
+    Title, Body string
+    URL         *url.URL
+    Errors      map[string]string
+}
+
+// 表单验证
+func validateArticleFormData(title string, body string) map[string]string {
+    errors := make(map[string]string)
+    // 验证标题
+    if title == "" {
+        errors["title"] = "标题不能为空"
+    } else if utf8.RuneCountInString(title) < 3 || utf8.RuneCountInString(title) > 40 {
+        errors["title"] = "标题长度需介于 3-40"
+    }
+
+    // 验证内容
+    if body == "" {
+        errors["body"] = "内容不能为空"
+    } else if utf8.RuneCountInString(body) < 10 {
+        errors["body"] = "内容长度需大于或等于 10 个字节"
+    }
+
+    return errors
+}
