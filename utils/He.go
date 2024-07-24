0b167eb1efd456767411a8d9c319633ad6c6fbf6
hejiahui <3044662106@qq.com>
2024-07-24 09:15:55 +0800

    提交

--- utils/He.go
+++ utils/He.go
@@ -882,3 +882,23 @@ func saveArticleToDB(title string, body string) (int64, error) {
 
     return 0, err
 }
+
+func forceHTMLMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // 1. 设置标头
+        w.Header().Set("Content-Type", "text/html; charset=utf-8")
+        // 2. 继续处理请求
+        next.ServeHTTP(w, r)
+    })
+}
+func removeTrailingSlash(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        // 1. 除首页以外，移除所有请求路径后面的斜杆
+        if r.URL.Path != "/" {
+            r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+        }
+
+        // 2. 将请求传递下去
+        next.ServeHTTP(w, r)
+    })
+}
